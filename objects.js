c138398e5ffb22ca9e91a79c0dbca8d95ccf9c92
Aleksey <fduchunru@gmail.com>
2020-09-28 02:55:07 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,10 +2,12 @@ var playlist = {
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes'
 };
-var artistName = "Phil Ochs";
-var songTitle = "Here's to the State of Mississippi";
-
-
+//var artistName = "Phil Ochs";
+//var songTitle = "Here's to the State of Mississippi";
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle
+  playlist[keyartistName] = songTitle
+
+  return playlist
 }
+updatePlaylist(playlist, "Phil Ochs", "Here's to the State of Mississippi")
+console.log(playlist)
